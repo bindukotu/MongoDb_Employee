2ba342b5203b997d9ed3a28a73d30a8c61bb197b
BinduKotu15285 <bindustudy85@gmail.com>
2025-01-28 12:53:12 +0530

    modify

--- src/main/java/com/faang/bindu/controller/CustomerController.java
+++ src/main/java/com/faang/bindu/controller/CustomerController.java
@@ -3,9 +3,10 @@ package com.faang.bindu.controller;
 import com.faang.bindu.entity.Customer;
 import com.faang.bindu.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CustomerController {
@@ -17,4 +18,33 @@ public class CustomerController {
         ser.insertData(cust);
         return "Record inserted --->"+cust.getCno();
     }
+
+    @PostMapping("/insertall")
+    public  String insertAllRecordes(@RequestBody List<Customer> cust){
+        ser.insertAllRecordes(cust);
+        return "all recodes added";
+    }
+
+    @GetMapping("/getbyid")
+    public Optional<Customer> findValue(@RequestBody Customer cust){
+          return ser.findValue(cust.getCno());
+    }
+
+    @GetMapping("/getall")
+    public List<Customer> getAllRecord(){
+        return ser.getAllRecord();
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteData(@RequestBody Customer cus){
+        ser.deleteData(cus.getCno());
+          return "Record deleted :"+cus.getCno();
+    }
+
+    @DeleteMapping("/deleteall")
+    public String deleteAllData(){
+        ser.deleteAllData();
+        return "record detleted All Recordes : ";
+    }
+
 }
--- src/main/java/com/faang/bindu/service/CustomerService.java
+++ src/main/java/com/faang/bindu/service/CustomerService.java
@@ -4,6 +4,10 @@ import com.faang.bindu.entity.Customer;
 import com.faang.bindu.repository.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -12,5 +16,27 @@ public class CustomerService {
 
     public void insertData(Customer cust){
         repo.insert(cust);
+    }
+
+    public void insertAllRecordes(@RequestBody List<Customer> cust){
+        repo.insert(cust);
+    }
+
+    public  Optional<Customer> findValue(Integer id){
+        Optional<Customer> cus=repo.findById(id);
+        return cus;
+    }
+
+    public List<Customer>  getAllRecord(){
+        List<Customer> li=repo.findAll();
+        return li;
+    }
+
+    public void deleteData( Integer id){
+        repo.deleteById(id);
+    }
+
+    public void deleteAllData(){
+        repo.deleteAll();
     }
 }
